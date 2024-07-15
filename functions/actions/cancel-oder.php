9eedf20f7446bd73f6e9105c7bcaf67f962559ee
makiomar <mo7amed.maki@gmail.com>
2024-07-15 15:40:04 +0300

    Cancel order cron

---
+++ functions/actions/cancel-oder.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * Cancel order
+ *
+ * @package Shrinks
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit();
+}
+
+if ( ! function_exists( 'WC' ) ) {
+	return;
+}
+
+/**
+ * Define cron interval
+ *
+ * @param array $schedules Schedules.
+ * @return array
+ */
+function register_custom_cron_intervals( $schedules ) {
+	$schedules['custom_15_minutes'] = array(
+		'interval' => 900,
+		'display'  => __( 'Every 15 Minutes' ),
+	);
+	return $schedules;
+}
+add_filter( 'cron_schedules', 'register_custom_cron_intervals' );
+
+/**
+ * Hook to schedule
+ *
+ * @return void
+ */
+function schedule_autocancel_wc_orders() {
+	if ( ! wp_next_scheduled( 'autocancel_wc_orders_event' ) ) {
+		wp_schedule_event( time(), 'custom_15_minutes', 'autocancel_wc_orders_event' );
+	}
+}
+add_action( 'wp', 'schedule_autocancel_wc_orders' );
+
+/**
+ * Cancel pending orders
+ *
+ * @return void
+ */
+function snks_auto_cancel_wc_orders() {
+	$query = array(
+		'limit'   => 5,
+		'orderby' => 'date',
+		'order'   => 'DESC',
+		'status'  => array( 'pending', 'on-hold', 'processing' ),
+	);
+
+	$orders = wc_get_orders( $query );
+	foreach ( $orders as $order ) {
+		$date     = new DateTime( $order->get_date_created() );
+		$now      = new DateTime();
+		$interval = $date->diff( $now );
+
+		$minutes_diff = $interval->format( '%i' );
+		if ( $minutes_diff > 10 ) {
+			$order->update_status( 'cancelled', 'Cancelled for missing payment' );
+			$booking_id = get_post_meta( $order->get_id(), 'booking_id', true );
+			$updated    = snks_update_timetable(
+				absint( $booking_id ),
+				array(
+					'order_id' => 0,
+				)
+			);
+			if ( $updated ) {
+				delete_post_meta( $order->get_id(), 'booking_id' );
+			}
+		}
+	}
+}
+
+add_action( 'autocancel_wc_orders_event', 'snks_auto_cancel_wc_orders' );
