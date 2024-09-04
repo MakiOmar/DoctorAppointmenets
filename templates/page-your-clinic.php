264478976a638554bbf7e6bc1c0d4f6d73ec652e
makiomar <mo7amed.maki@gmail.com>
2024-09-04 19:37:42 +0300

    Move css to styles

--- templates/page-your-clinic.php
+++ templates/page-your-clinic.php
@@ -11,143 +11,6 @@ get_header();
 $user_id      = snks_url_get_doctors_id();
 $user_details = snks_user_details( $user_id );
 ?>
-<style>
-	#snks-booking-page{
-		overflow: hidden;
-	}
-	.snks-tear-shap-wrapper{
-		width: 200px;
-		height: 200px;
-		margin: auto;
-		margin-top: 50px;
-		transform: rotate(45deg);
-	}
-	.snks-tear-shap{
-		width: 100%;
-		height: 100%;
-		border-radius: 50%;
-		border-top-right-radius: 0;
-		transform: rotate(-45deg);
-		overflow: hidden;
-		background-color: #fff;
-	}
-	.snks-tear-shap.sub{
-		position: absolute;
-		right: 0px;
-		top: 13px;
-		z-index: -1;
-	}
-	.snks-tear-shap img{
-		height: 203px;
-		width: 203px;
-	}
-	.snks-profile-image-wrapper{
-		position: relative;
-		max-width: 350px;
-		margin: auto;
-	}
-	#head1{
-		top: -40px;
-	left: 90px;
-	}
-	#head2{
-		bottom: -20px;
-		left: 40px;
-	}
-	#head3{
-		bottom: 50px;
-	right: 20px;
-	}
-	.shap-head{
-		position: absolute;
-		height: 45px;
-	}
-	.profile-details{
-		margin-top: 20px;
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-	}
-	.profile-details h1,.profile-details h2{
-		margin: 5px 0;
-		color:#024059
-	}
-	@keyframes move_wave {
-		0% {
-			transform: translateX(0) translateZ(0) scaleY(1)
-		}
-		50% {
-			transform: translateX(-25%) translateZ(0) scaleY(0.55)
-		}
-		100% {
-			transform: translateX(-50%) translateZ(0) scaleY(1)
-		}
-	}
-	.waveWrapper {
-		overflow: hidden;
-		position: absolute;
-		left: 0;
-		right: 0;
-		bottom: 0;
-		top: 0;
-		margin: auto;
-	}
-	.waveWrapperInner {
-		position: absolute;
-		width: 100%;
-		overflow: hidden;
-		height: 100%;
-		bottom: -1px;
-		background: #024059;
-	}
-	.bgTop {
-		z-index: 15;
-		opacity: 0.5;
-	}
-	.bgMiddle {
-		z-index: 10;
-		opacity: 0.75;
-	}
-	.bgBottom {
-		z-index: 5;
-	}
-	.wave {
-		position: absolute;
-		left: 0;
-		width: 800%;
-		height: 100%;
-		background-repeat: repeat no-repeat;
-		background-position: 0 bottom;
-		transform-origin: center bottom;
-	}
-	.waveTop {
-		background-size: 50% 100px;
-	}
-	.waveAnimation .waveTop {
-		animation: move-wave 3s;
-		-webkit-animation: move-wave 3s;
-		-webkit-animation-delay: 1s;
-		animation-delay: 1s;
-	}
-	.waveMiddle {
-		background-size: 50% 120px;
-	}
-	.waveAnimation .waveMiddle {
-		animation: move_wave 10s linear infinite;
-	}
-	.waveBottom {
-		background-size: 50% 100px;
-	}
-	.waveAnimation .waveBottom {
-		animation: move_wave 15s linear infinite;
-	}
-	.waveWrapper-containr{
-		position: relative;
-		height: 100px;
-		transform: rotate(180deg);
-	}
-</style>
 <div id="snks-booking-page">
 	<?php
 	if ( ! $user_id ) {
@@ -190,23 +53,11 @@ $user_details = snks_user_details( $user_id );
 			?>
 		</div>
 	</div>
-	<div style="background-color:#fff;display: flex;justify-content: center;padding-top: 10px;">
-		<h3 style="margin:0;">إحجز جلسة</h3>
+	<div style="background-image:url('/wp-content/uploads/2024/09/teeth.png');background-repeat:no-repeat; background-position:center;background-size:cover; display: flex;justify-content: center;padding: 10px;">
+		<h3 class="snks-light-bg">إحجز جلسة</h3>
 	</div>
-	<div class="waveWrapper-containr">
-		<div class="waveWrapper waveAnimation">
-			<div class="waveWrapperInner bgTop"  style="display:none;">
-			<div class="wave waveTop" style="background-image: url('https://jalsah.app/wp-content/uploads/2024/09/wave-top.png')"></div>
-			</div>
-			<div class="waveWrapperInner bgMiddle"  style="display:none;">
-			<div class="wave waveMiddle" style="background-image: url('https://jalsah.app/wp-content/uploads/2024/09/wave-mid.png')"></div>
-			</div>
-			<div class="waveWrapperInner bgBottom">
-			<div class="wave waveBottom" style="background-image: url('https://jalsah.app/wp-content/uploads/2024/09/wave-bot.png')"></div>
-			</div>
-		</div>
-	</div>
-	<div id="snks-booking-form" class="anony-grid-row" style="max-width: 960px;margin:auto;position: relative;top: -1px;">
+
+	<div id="snks-booking-form" class="anony-grid-row" style="max-width: 960px;margin:auto;position: relative;">
 			<?php echo do_shortcode( '[snks_appointment_form]' ); ?>
 	</div>
 	<?php } ?>
