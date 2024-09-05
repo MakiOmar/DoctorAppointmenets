c36a70ae4cea6ac008a4ded00908e3b066e83a5f
makiomar <mo7amed.maki@gmail.com>
2024-09-05 14:25:56 +0300

    css

--- functions/styles.php
+++ functions/styles.php
@@ -607,38 +607,8 @@ add_action(
 			}
 		</style>
 		<style>
-			#todo-pages-container{
-				position: relative;
-				margin-top: 20px;
-			}
-			#todo-pages-container .anony-content-slider-control{
-				top: -30px;
-			}
-			#to-do-form{
-				padding-bottom: 100px;
-			}
-			#to-do-form input[type=submit]{
-				position: fixed;
-				bottom: 0px;
-				left: 0;
-				right: 0;
-				margin: auto;
-			}
-			#to-do-form table tbody td{
-				border: none;
-				outline: none;
-				text-align: center;
-				vertical-align: middle;
-				padding: 5px;
-			}
-			#to-do-form table tbody tr td:nth-child(1) {
-				border-left: 1px solid #e1e1e1;
-			}
-			.todo-page{
-				display: none;
-			}
-			.todo-page.active-todo-page{
-				display: block;
+			.slick-list {
+				max-width: 85vw!important;
 			}
 			#call-customer-care{
 				position: fixed;
@@ -683,8 +653,8 @@ add_action(
 				display: none;
 			}
 			.anony-day-radio label.active-day, input[type=submit]{
-				background-color: #024059;
-				color:#fff;
+				background-color: #fff;
+				color:#024059;
 				border:none
 			}
 			#snks-available-hours-wrapper{
@@ -699,39 +669,37 @@ add_action(
 				display: inline-flex;
 				justify-content: center;
 				align-items: center;
-				padding: 5px 10px;
 				border-radius: 5px;
-				margin-left: 10px;
+				font-size: 15px;
 			}
-			.snks-available-hours li.available-time{
+			.snks-available-hours li.available-time label{
 				background-color: #024059;
 				color: #fff;
-				border-radius: 50px;
-				padding: 5px 15px;
-				width: 30%;
+				border-radius: 8px;
+				padding: 10px;
 				box-sizing: border-box;
 				display: inline-flex;
 				justify-content: center;
 				align-items: center;
 				margin-bottom: 10px;
 				border:1px solid #fff;
+				overflow: hidden;
 
 			}
-			.snks-available-hours li.available-time label{
+			.snks-available-hours ul{
 				display: flex;
-				flex-direction: column;
-				justify-content: center;
-				align-items: center;
-				width: 100%;
-				height: 100%;
+				flex-wrap: wrap;
+				max-width: 90vw;
 			}
 			.snks-available-hours li.available-time input{
 				display:none
 			}
-			.snks-available-hours li.available-time.active-hour{
+			.snks-available-hours li.available-time.active-hour label{
 				background-color: #fff;
 				color: #024059;
-				border:1px solid #024059;
+			}
+			#consulting-form-submit{
+				display: none;
 			}
 			.snks-form .row{
 				margin-bottom: 15px;
@@ -807,45 +775,41 @@ add_action(
 				font-size: 12px;
 			}
 			/**Content slider */
-			.anony-content-slider-container {
-				position: relative;
-				overflow: hidden;
-				margin: auto;
-				width: 319px;
-			}
-			.anony-content-slider div{
-				max-width: 100vw;
-			}
-			.anony-content-slider-control{
-				position: absolute;
-				top: -5px;
-				left: 0;
-				text-align: center;
-			}
-			body.rtl .anony-content-slider-control{
-				display: flex;
-				justify-content: center;
-				align-items: center;
-				flex-direction: row-reverse;
+
+			.slick-slide {
+				margin: 0 5px;
 			}
 
-			.anony-content-slide {
-				display: inline-block;
-				vertical-align: top;
-				margin: 0;
+			.slick-prev, .slick-next {
+				font-size: 0!important;
+				line-height: 0;
+				position: absolute;
+				top: 50%;
+				display: block;
+				width: 20px;
+				height: 20px;
+				padding: 0;
+				cursor: pointer;
+				color: transparent;
+				border: none;
+				outline: none;
+				background: transparent!important;
+				z-index: 12;
 			}
+			.slick-prev::before, .slick-next::before {
+				font-family: 'slick';
+				font-size: 23px;
+				line-height: 1;
+				opacity:1;
+				color: #024059;
+				-webkit-font-smoothing: antialiased;
+				-moz-osx-font-smoothing: grayscale;
+				cursor: pointer;
+			}
+			
 			.anony-day-number{
 				font-size: 27px;
 				margin: 8px;
-			}
-
-			
-			.anony-content-slider-nav .top, .anony-content-slider-nav .bottom {
-				display: block;
-				width: 10px;
-				height: 1px;
-				height: 1px;
-				background-color: #fff;
 			}
 
 			.anony-greater-than .top {
@@ -867,32 +831,10 @@ add_action(
 			.anony-smaller-than .bottom {
 				transform: rotate(45deg);
 			}
-			.anony-content-slider-control .button, #todo-pages-prev, #todo-pages-next{
-				height: 25px;
-				width: 25px;
-				margin: 0 3px;
-				background-color: #024059;
-				color: #fff;
-				outline: none;
-				border-radius: 50%;
-				border: none;
-				cursor: pointer;
-				display: flex;
-				justify-content: center;
-				align-items: center;
+			
+			.anony-content-slide{
+				max-width: 60px!important;
 			}
-			.anony-content-slider-control .button:hover{
-				background-color: rgb(0,0,0,1);
-			}
-			.anony-content-slider-nav{
-				position: relative;
-				top: -3px;
-				display: flex;
-				flex-direction: column;
-				justify-content: center;
-				align-items: center;
-			}
-
 			@media screen and (min-width:481px) {
 				.offline-clinic-details{
 					width:25%
@@ -902,9 +844,6 @@ add_action(
 				.clinc-row{
 					flex-direction: column;
 					gap: 10px;
-				}
-				.anony-content-slider div{
-					max-width: calc(100vw - 40px);
 				}
 				.anony-content-slide .wp-block-columns{
 					flex-direction: column;
