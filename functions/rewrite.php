4e3ce1845cfdf8b2f614593931073d1b9b969089
makiomar <mo7amed.maki@gmail.com>
2024-09-07 16:00:25 +0300

    Create rewrite rule for organization

--- functions/rewrite.php
+++ functions/rewrite.php
@@ -16,6 +16,7 @@ if ( ! defined( 'ABSPATH' ) ) {
  */
 function snks_rewrite_rule() {
 	add_rewrite_rule( '^7jz/(\w+)/?', 'index.php?doctor_id=$matches[1]', 'top' );
+	add_rewrite_rule( '^org/(\w+)/(\d+)/?', 'index.php?term_id=$matches[1]', 'top' );
 }
 add_action( 'init', 'snks_rewrite_rule' );
 
@@ -23,6 +24,7 @@ add_filter(
 	'query_vars',
 	function ( $query_vars ) {
 		$query_vars[] = 'doctor_id';
+		$query_vars[] = 'term_id';
 		return $query_vars;
 	}
 );
@@ -30,11 +32,27 @@ add_filter(
 	'template_include',
 	function ( $template ) {
 		$user_id = get_query_var( 'doctor_id' );
+		$term_id = get_query_var( 'term_id' );
+
+		if ( $term_id ) {
+			return SNKS_DIR . 'templates/page-org-doctors.php';
+		}
 
 		if ( $user_id ) {
 			return SNKS_DIR . 'templates/page-your-clinic.php';
 		}
 
 		return $template;
+	}
+);
+add_action(
+	'template_redirect',
+	function () {
+		global $wp;
+		//phpcs:disable
+		if ( strpos( $_SERVER['REQUEST_URI'], '7jz' ) !== false && ( ! isset( $wp->query_vars ) || empty( $wp->query_vars['doctor_id'] ) ) ) {
+			//wp_redirect( site_url() );
+			//exit;
+		}
 	}
 );
---
+++ templates/page-org-doctors.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Template Name: Your Clinic
+ *
+ * @package Shrinks
+ */
+
+defined( 'ABSPATH' ) || die;
+
+get_header();
+
+?>
+<div class="snks-booking-page-container">
+	<?php echo 'jjjjjjjjjjjjjjjjjj' ?>
+</div>
+<?php
+get_footer();
