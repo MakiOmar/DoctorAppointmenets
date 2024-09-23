7a5dc4be95b099c297c5f609652335a55501a08f
makiomar <mo7amed.maki@gmail.com>
2024-09-23 12:30:39 +0300

    Override checkout template

---
+++ functions/templates.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Accounting
+ *
+ * @package Nafea
+ */
+
+defined( 'ABSPATH' ) || die();
+// Hook into the template loader.
+add_filter( 'woocommerce_locate_template', 'snks_thank_you_template', 10, 3 );
+
+/**
+ * Replace WooCommerce Thank You page template.
+ *
+ * @param string $template      The path of the template to load.
+ * @param string $template_name The name of the template.
+ *
+ * @return string
+ */
+function snks_thank_you_template( $template, $template_name ) {
+	// Define the plugin template directory.
+	$plugin_path = SNKS_DIR . 'woocommerce/';
+	// Check if we're looking for the thankyou template.
+	if ( 'checkout/thankyou.php' === $template_name ) {
+		// If our custom template exists, load it instead of the default.
+		if ( file_exists( $plugin_path . $template_name ) ) {
+			$template = $plugin_path . $template_name;
+		}
+	}
+
+	return $template;
+}
+
+add_action( 'template_redirect', 'remove_order_details_from_thank_you_page' );
+/**
+ * Remove order details
+ *
+ * @return void
+ */
+function remove_order_details_from_thank_you_page() {
+	if ( is_wc_endpoint_url( 'order-received' ) ) {
+		remove_action( 'woocommerce_thankyou', 'woocommerce_order_details_table', 10 );
+	}
+}
---
+++ woocommerce/checkout/thankyou.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Thankyou page
+ *
+ * This template can be overridden by copying it to yourtheme/woocommerce/checkout/thankyou.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see https://woocommerce.com/document/template-structure/
+ * @package WooCommerce\Templates
+ * @version 8.1.0
+ *
+ * @var WC_Order $order
+ */
+
+defined( 'ABSPATH' ) || die();
+
+?>
+<div class="woocommerce-order">
+
+	<?php
+	if ( $order ) :
+
+		if ( $order->has_status( 'failed' ) ) {
+			echo '';
+		} else {
+			echo do_shortcode( '[elementor-template id="3761"]' );
+		}
+		do_action( 'woocommerce_thankyou', $order->get_id() );
+	endif;
+	?>
+
+</div>
