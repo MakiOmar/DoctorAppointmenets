748def09f7a2db8a6f0562cbbdfcfef416a8fb27
makiomar <mo7amed.maki@gmail.com>
2024-11-20 09:51:13 +0200

    Load clinics based on uuid

--- classes/class-snks-get-clinics.php
+++ classes/class-snks-get-clinics.php
@@ -54,7 +54,7 @@ class Snks_Get_Clinics extends Base {
 		);
 		if ( ! empty( $clinics_meta ) ) {
 			foreach ( $clinics_meta as $index => $clinic ) {
-				if ( empty( $clinic['uuid'] ) || 'on' === $clinic['disabled'] ) {
+				if ( empty( $clinic['uuid'] ) || ( isset( $clinic['disabled'] ) && 'on' === $clinic['disabled'] ) ) {
 					continue;
 				}
 				$result[] = array(
