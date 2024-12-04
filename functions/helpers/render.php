95cbcbfa19e0218e65b4e99fdf3c670f1471120c
makiomar <mo7amed.maki@gmail.com>
2024-12-04 10:02:47 +0200

    Refactor preview table

--- functions/helpers/render.php
+++ functions/helpers/render.php
@@ -188,8 +188,8 @@ function snks_generate_preview() {
 					)
 				);
 				// Create table columns with a column key and column object.
-				$table->addColumn( 'day', new TableColumn( 'اليوم' ) );
-				$table->addColumn( 'datetime', new TableColumn( 'التاريخ والوقت' ) );
+				$table->addColumn( 'day', new TableColumn( 'تبدأ من' ) );
+				//$table->addColumn( 'datetime', new TableColumn( 'التاريخ والوقت' ) );
 				// $table->addColumn( 'starts', new TableColumn( 'تبدأ من' ) );
 				$table->addColumn( 'ends', new TableColumn( 'تنتهي عند' ) );
 				$table->addColumn( 'period', new TableColumn( 'المدة' ) );
@@ -206,7 +206,7 @@ function snks_generate_preview() {
 					if ( count( $details ) > 1 || count( $details ) == 1 ) {
 						// Associate cells with columns.
 						$cells = array(
-							'day' => new TableCell( snks_localize_day( $day ) . ' ' . $date, array( 'colspan' => '6' ) ),
+							'day' => new TableCell( snks_localize_day( $day ) . ' ' . $date, array( 'colspan' => '5' ) ),
 						);
 						// define row attributes.
 						$attrs = array(
@@ -236,8 +236,8 @@ function snks_generate_preview() {
 						}
 						// Associate cells with columns.
 						$cells = array(
-							'day'        => new TableCell( snks_localize_day( $data['day'] ), array( 'data-label' => 'اليوم' ) ),
-							'datetime'   => new TableCell( $date . ' ' . snks_localize_time( gmdate( 'h:i a', strtotime( $data['starts'] ) ) ), array( 'data-label' => 'التاريخ والوقت' ) ),
+							'day'        => new TableCell( snks_localize_time( gmdate( 'h:i a', strtotime( $data['starts'] ) ) ), array( 'data-label' => 'تبدأ من' ) ),
+							//'datetime'   => new TableCell( $date . ' ' . snks_localize_time( gmdate( 'h:i a', strtotime( $data['starts'] ) ) ), array( 'data-label' => 'التاريخ والوقت' ) ),
 							// 'starts'     => new TableCell( snks_localize_time( gmdate( 'h:i a', strtotime( $data['starts'] ) ) ), array( 'data-label' => 'تبدأ من' ) ),
 							'ends'       => new TableCell( snks_localize_time( gmdate( 'h:i a', strtotime( $data['ends'] ) ) ), array( 'data-label' => 'تنتهي عند' ) ),
 							'period'     => new TableCell( $data['period'], array( 'data-label' => 'المدة' ) ),
