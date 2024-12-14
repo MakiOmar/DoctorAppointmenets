7024164642ebda9e2d051b029b1a912f75a23686
makiomar <mo7amed.maki@gmail.com>
2024-12-14 09:39:02 +0200

    css

--- functions/styles.php
+++ functions/styles.php
@@ -1632,11 +1632,6 @@ add_action(
 			.snks_tabs-container {
 				display: flex;
 				width: 100%;
-				z-index: 99999999;
-				position: fixed;
-				bottom: 0;
-				left: 0;
-				right: 0;
 				width: 100%
 			}
 
