36f116088b772e714d4c6629c1f3e28bfecd7038
makiomar <mo7amed.maki@gmail.com>
2025-01-26 13:45:47 +0200

    Fix next scheduled

--- functions/crons/permalinks.php
+++ functions/crons/permalinks.php
@@ -7,7 +7,7 @@
 
 defined( 'ABSPATH' ) || die();
 
-if ( ! wp_next_scheduled( 'flush_rewrite_cron_job' ) ) {
+if ( ! wp_next_scheduled( 'snks_flush_rewrite_cron_job' ) ) {
 	wp_schedule_event( time(), 'hourly', 'snks_flush_rewrite_cron_job' );
 }
 add_action(
