f96161d580a198f37f922f025ce3d9f5d23a3be9
makiomar <mo7amed.maki@gmail.com>
2025-02-01 10:42:30 +0200

    Check user

--- functions/crons/sms.php
+++ functions/crons/sms.php
@@ -58,7 +58,7 @@ function snks_send_session_notifications() {
 		$time_diff     = strtotime( $session->date_time ) - strtotime( $current_time );
 		$billing_phone = get_user_meta( $session->client_id, 'billing_phone', true );
 		$user          = get_user_by( 'id', $session->client_id );
-		if ( empty( $billing_phone ) ) {
+		if ( empty( $billing_phone ) && $user ) {
 			$billing_phone = $user->user_login;
 		}
 		if ( ! empty( $billing_phone ) ) {
