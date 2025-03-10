d07d2171083b8d08476f87dced34f863f29007ee
makiomar <mo7amed.maki@gmail.com>
2025-03-10 10:46:59 +0200

    Change pixel script

--- functions/scripts.php
+++ functions/scripts.php
@@ -49,44 +49,6 @@ add_action(
 );
 
 add_action(
-	'wp_head',
-	function () {
-		//phpcs:disable
-		if ( false === strpos( $_SERVER['REQUEST_URI'], '/org/' ) ) {
-			return;
-		}
-		//phpcs:enable
-		?>
-		<!-- Meta Pixel Code -->
-		<script>
-			(function(f, b, e, v, n, t, s) {
-				if (f.fbq) return;
-				n = f.fbq = function() {
-					n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments);
-				};
-				if (!f._fbq) f._fbq = n;
-				n.push = n;
-				n.loaded = !0;
-				n.version = '2.0';
-				n.queue = [];
-				t = b.createElement(e);
-				t.async = !0;
-				t.src = v;
-				s = b.getElementsByTagName(e)[0];
-				s.parentNode.insertBefore(t, s);
-			})(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
-			fbq('init', '1256762418664304');
-			fbq('track', 'PageView');
-		</script>
-		<noscript>
-			<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=1256762418664304&ev=PageView&noscript=1"/>
-		</noscript>
-		<!-- End Meta Pixel Code -->
-		<?php
-	},
-	1
-);
-add_action(
 	'wp_footer',
 	function () {
 		?>
@@ -783,7 +745,9 @@ add_action(
 add_action(
 	'wp_head',
 	function () {
-		if ( is_page( 'doctor-login' ) || is_page( 'register' ) ) {
+		//phpcs:disable
+		if ( is_page( 'doctor-login' ) || is_page( 'register' ) || is_page( 5170 ) || ( isset( $_SERVER['REQUEST_URI'] ) && false !== strpos( $_SERVER['REQUEST_URI'], '/org/' ) ) ) {
+			//phpcs:enable
 			?>
 			<!-- Meta Pixel Code -->
 			<script>
@@ -795,11 +759,11 @@ add_action(
 			t.src=v;s=b.getElementsByTagName(e)[0];
 			s.parentNode.insertBefore(t,s)}(window, document,'script',
 			'https://connect.facebook.net/en_US/fbevents.js');
-			fbq('init', '1001541981792508');
+			fbq('init', '572699531895875');
 			fbq('track', 'PageView');
 			</script>
 			<noscript><img height="1" width="1" style="display:none"
-			src="https://www.facebook.com/tr?id=1001541981792508&ev=PageView&noscript=1"
+			src="https://www.facebook.com/tr?id=572699531895875&ev=PageView&noscript=1"
 			/></noscript>
 			<!-- End Meta Pixel Code -->
 			<?php
