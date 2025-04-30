308c5e543140c37a5d777a1001930505e5b333ba
makiomar <mo7amed.maki@gmail.com>
2025-04-30 13:27:39 +0300

    Version upgrade

--- anony-shrinks.php
+++ anony-shrinks.php
@@ -3,7 +3,7 @@
  * Plugin Name: A Shrinks
  * Plugin URI: https://makiomar.com/
  * Description: Shrinks Clinics
- * Version: 0.131
+ * Version: 0.132
  * Author: Makiomar
  * Author URI: https://makiomar.com/
  * License: GPLv2 or later
