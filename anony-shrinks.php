fdda8ea52c929c2900d227c9cb077618a9a5e306
TaskFlow Developer <developer@taskflow.app>
2025-12-14 12:49:31 +0200

    Version upgrade to 1.0.205 - Added welcome message setting option

--- anony-shrinks.php
+++ anony-shrinks.php
@@ -3,7 +3,7 @@
  * Plugin Name: A Shrinks
  * Plugin URI: https://makiomar.com/
  * Description: Shrinks Clinics
- * Version: 1.0.204
+ * Version: 1.0.205
  * Author: Makiomar
  * Author URI: https://makiomar.com/
  * License: GPLv2 or later
