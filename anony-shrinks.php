a8420f60d77eccce98b5d319a2026a350b54a65b
TaskFlow Developer <developer@taskflow.app>
2026-06-22 17:09:05 +0300

    Vesrion upgrade

--- anony-shrinks.php
+++ anony-shrinks.php
@@ -3,7 +3,7 @@
  * Plugin Name: A Shrinks
  * Plugin URI: https://makiomar.com/
  * Description: Shrinks Clinics
- * Version: 1.0.354
+ * Version: 1.0.355
  * Author: Makiomar
  * Author URI: https://makiomar.com/
  * License: GPLv2 or later
