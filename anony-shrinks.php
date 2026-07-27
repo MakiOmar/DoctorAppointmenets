dd224160229c39b36278adb88149bd66c4e58b66
TaskFlow Developer <developer@taskflow.app>
2026-07-27 17:14:27 +0300

    Version upgrade

--- anony-shrinks.php
+++ anony-shrinks.php
@@ -3,7 +3,7 @@
  * Plugin Name: A Shrinks
  * Plugin URI: https://makiomar.com/
  * Description: Shrinks Clinics
- * Version: 1.0.362
+ * Version: 1.0.363
  * Author: Makiomar
  * Author URI: https://makiomar.com/
  * License: GPLv2 or later
